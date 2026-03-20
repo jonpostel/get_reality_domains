f2cda952078551843500766117fe3f0f440ce0bc
vocaloidat <vocaloid.at@gmail.com>
2026-03-20 20:22:52 +0800

    输出终端

--- get_reality_domains.sh
+++ get_reality_domains.sh
@@ -4,7 +4,6 @@ curl -sL "https://raw.githubusercontent.com/mack-a/v2ray-agent/master/install.sh
 
 domain_list=$(sed -n '9245p' /tmp/install.sh | sed 's/.*realityDestDomainList="\([^"]*\)".*/\1/')
 
-echo "$domain_list" | tr ',' '\n' > RealityName.txt
+echo "$domain_list" | tr ',' '\n'
 
-echo "已成功将 xray-core 的域名写入 RealityName.txt"
-echo "共 $(wc -l < RealityName.txt) 个域名"
+echo "共 $(echo "$domain_list" | tr ',' '\n' | wc -l) 个域名"
